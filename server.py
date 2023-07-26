15a5ea7cd9b1618f815adb8411f1d6a265b1c174
Mark Terrel <mark@adaptable.io>
2023-07-25 18:12:29 -0600

    Debug: show python version

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import os
 from flask import Flask, render_template
+import sys
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print("Python version", flush=True)
+    print(sys.version, flush=True)
     return render_template('index.html')
 
 if __name__ == '__main__':
